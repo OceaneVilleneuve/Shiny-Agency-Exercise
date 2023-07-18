81e2703bbf7eb370c969e728a84da1ca58ec8641
Oceane Villeneuve <oceanevilleneuve16@gmail.com>
2023-07-18 11:42:54 +0200

    adding css to my home, freelancers and error pages

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
 import DefaultPicture from '../../assets/profile.png'
 import styled from 'styled-components'
+import colors from '../../utils/style/colors'
 
 const CardLabel = styled.span``
 const CardImage = styled.img`
@@ -8,16 +9,30 @@ const CardImage = styled.img`
     width: 80px;
     border-radius: 50%;
 `
+const CardWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 15px;
+    background-color: ${colors.backgroundLight};
+    border-radius: 30px;
+    width: 350px;
+    transition: 200ms;
+    &:hover {
+        cursor: pointer;
+        box-shadow: 2px 2px 10px #e2e3e9;
+    }
+`
 
 function Card({ label, title, picture }) {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
-      <CardLabel>{label}</CardLabel>
-      <CardImage src={picture} alt="freelance" />
-      <span>{title}</span>
-    </div>
+      <CardWrapper>
+          <CardLabel>{label}</CardLabel>
+          <CardImage src={picture} alt="freelance" />
+          <span>{title}</span>
+      </CardWrapper>
   )
 }
+
 
 Card.propTypes = {
   label: PropTypes.string.isRequired,
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,64 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { StyledLink } from '../../utils/style/Atoms'
+import colors from '../../utils/style/colors'
+import HomeIllustration from '../../assets/home-illustration.svg'
+
+const HomeBackgroundImage = styled.img`
+  flex: 1;
+`
+const StyledTitle = styled.h2`
+  padding-bottom: 30px;
+  max-width: 280px;
+  line-height: 50px;
+`
+
+const HomeWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+
+const HomeContainer = styled.nav`
+margin: 30px;
+background-color: ${colors.backgroundLight};
+padding: 60px 90px;
+display: flex;
+flex-direction: row;
+max-width: 1200px;
+`
+
+const LeftCol = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex: 1;
+  margin-right: 2em;
+  ${StyledLink} {
+    max-width: 250px;
+  }
+`
+
+
 function Home() {
-  return <div>Page d'accueil 🏡</div>
+  return (
+    <HomeWrapper>
+      <HomeContainer>
+        <LeftCol>
+          <StyledTitle>
+            Repérez vos besoins, on s'occupe du reste,
+            avec les meilleurs talents
+          </StyledTitle>
+          <StyledLink to="/survey/1" $isFullLink>
+              Faire le test
+          </StyledLink>
+        </LeftCol>
+        <HomeBackgroundImage src={HomeIllustration} />
+      </HomeContainer>
+    </HomeWrapper>
+
+  )
+
 }
 
 export default Home
